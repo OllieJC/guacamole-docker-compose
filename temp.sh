1462bc29c1dd697e03d4a262805416ce0a3703fd
OllieJC <oliver.chalk@digital.cabinet-office.gov.uk>
2019-09-25 08:11:16 +0100

    Add localrdp dependancies

--- temp.sh
+++ temp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #
 docker build -t localrdp:latest -f Dockerfile-localrdp .
-docker run --name localrdp -dit --restart unless-stopped --network="guacamoledockercompose_guacnetwork_compose" localrdp
+docker run --name localrdp -dit --restart unless-stopped --network="guacamole-docker-compose_guacnetwork_compose" localrdp
